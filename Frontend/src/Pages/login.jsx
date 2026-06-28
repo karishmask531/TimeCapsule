b5407f74489ac0ae734fbed3b4010f1cd9dff943
Karishma Shaik <karishmask531@gmail.com>
2026-06-28 20:08:57 +0530

    login

--- Frontend/src/Pages/login.jsx
+++ Frontend/src/Pages/login.jsx
@@ -1,7 +1,27 @@
-function Login (){
+import { Link } from "react-router-dom";
+import "./login.css"
+function Login() {
     return (
-        <h1>Login Page
-        </h1>
+        <div className="Login-container">
+            <div className="logo-page">
+                <div className="logo-section">
+                    <h2>⏳ TimeCapsule</h2>
+                </div>
+                <div className="heading-section">
+                    <h1>Welcome Back 👋</h1>
+                    <p>Continue your journey by logging in.</p>
+                </div>
+                <form className="login-form">
+                    <label>Email address</label>
+                    <input type="email" placeholder="Enter email address" required />
+                    <label>Password</label>
+                    <input type="Password" placeholder="Password" required />
+                    <p className="forgot-password">forgot password</p>
+                    <button type="submit">login</button>
+                </form>
+                <p className="signup-link">Don't you have an account ? <Link to="/signup">Sign up</Link></p>
+            </div>
+        </div>
     )
 }
 export default Login;
